e462ef3721bd3929780f07b78a9e1598e1389584
José Romary Brandão <joseromarybrandao@gmail.com>
2022-12-01 23:56:43 -0300

    get data user data api github

--- src/Components/SummaryUser/index.tsx
+++ src/Components/SummaryUser/index.tsx
@@ -4,38 +4,63 @@ import githubIcon from '../../Assets/github.svg'
 import company from '../../Assets/company.svg'
 import followers from '../../Assets/followers.svg'
 import redirect from "../../Assets/IconRedirect.svg"
+import { useEffect, useState } from "react";
+import { api } from "../../Libs/axios";
+
+interface User {
+  name: string;
+  avatar_url: string
+  bio: string;
+  login: string;
+  company?: string;
+  followers: number;
+  html_url: string;
+}
+
 export default function SummaryUser() {
+
+  const [dataUser, setDataUser] = useState<User>(Object);
+
+  async function getDataUser() {
+    const response = await api.get('/users/jjromary')
+
+    setDataUser(response.data)
+  }
+
+  useEffect(() => {
+    getDataUser()
+  }, [])
+
   return (
     <SummaryUserContainer>
       <SummaryUserContent>
-
         <ContentLeft>
-          <img src={pacoca} style={{ width: "148px", height: "148px", borderRadius: '8px' }} />
-
+          <img src={dataUser.avatar_url} style={{ width: "148px", height: "148px", borderRadius: '8px' }} />
         </ContentLeft>
 
         <ContentRight>
           <RedirectContent>
-            <Title>José Romary Brandão </Title>
+            <Title>{dataUser.name} </Title>
             <RedirectButton>
               GITHUB
-              <a href="https://github.com/jjromary">
+              <a href={dataUser?.html_url}>
                 <img src={redirect} />
               </a>
             </RedirectButton>
           </RedirectContent>
-          <About>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
-          </About>
+          <About>{dataUser.bio}</About>
           <Infos>
             <InfosDetails>
-              <img src={githubIcon} />jjromary
+              <img src={githubIcon} />
+              {dataUser?.login}
             </InfosDetails>
             <InfosDetails>
-              <img src={company} />Romary Soluções Tencológicas
+              <img src={company} />
+              {dataUser?.company ? dataUser?.company : "Romary Soluções Tecnológicas"}
             </InfosDetails>
             <InfosDetails>
-              <img src={followers} />1 bilhão
+              <img src={followers} />
+              {dataUser.followers}
             </InfosDetails>
           </Infos>
         </ContentRight>
